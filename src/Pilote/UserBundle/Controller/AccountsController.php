0c7857703b2a1a5c5e796bdbac6b491cb4dbad36
akDevGit <akdevbox@gmail.com>
2017-05-21 22:34:17 +0200

    FOS User Bundle implementation -- 03

--- src/Pilote/UserBundle/Controller/AccountsController.php
+++ src/Pilote/UserBundle/Controller/AccountsController.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Pilote\UserBundle\Form\UserContactType;
 use Pilote\UserBundle\Form\UserInfosType;
 use Pilote\UserBundle\Form\UserTeamType;
+use FOS\UserBundle\Util\LegacyFormHelper;
 
 class AccountsController extends Controller
 {
@@ -26,11 +27,13 @@ class AccountsController extends Controller
         
         $formbuilder
             ->add('username', TextType::class)
-            ->add('password', TextType::class)
-            //->add('infos', UserInfosType::class)
-            //->add('contact', UserContactType::class)
-            //->add('team', UserTeamType::class)
+            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array('type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'), 'options' => array('translation_domain' => 'FOSUserBundle'), 'first_options' => array('label' => 'form.password'), 'second_options' => array('label' => 'form.password_confirmation'), 'invalid_message' => 'fos_user.password.mismatch',))
+            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('infos', UserInfosType::class)
+            ->add('contact', UserContactType::class)
+            ->add('team', UserTeamType::class)
             ->add('roles', ChoiceType::class, array('multiple' => true , 'choices' => $roles))
+            
             ->add('bip', SubmitType::class);
         
         $form = $formbuilder->getForm();
@@ -47,5 +50,12 @@ class AccountsController extends Controller
     }
         
         return $this->render('PiloteUserBundle:Accounts:creation.html.twig', array('form' => $form->createView()));
+    }
+    
+    public function usersListAction(Request $request) {
+        $repository = $this->getDoctrine()->getManager()->getRepository('PiloteUserBundle:User');
+        $usersList = $repository->findAll();
+        
+        return $this->render('PiloteUserBundle:Accounts:userList.html.twig', array('userList' => $usersList));
     }
 }
--- src/Pilote/UserBundle/Form/UserInfosType.php
+++ src/Pilote/UserBundle/Form/UserInfosType.php
@@ -5,6 +5,7 @@ namespace Pilote\UserBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Pilote\UserBundle\Form\UserGenderType;
 
 class UserInfosType extends AbstractType
 {
@@ -13,7 +14,7 @@ class UserInfosType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('firstname')->add('gender')->add('birthdate')->add('avatar')->add('avatarURL');
+        $builder->add('name')->add('firstname')->add('gender', UserGenderType::class, array('placeholder' => 'Choose a gender',))->add('birthdate')->add('avatar')->add('avatarURL');
     }
     
     /**
